99727703a0cd0d824bc34cc995bd82dd72bdd4f3
Sharonpa1 <94108962+Sharonpa1@users.noreply.github.com>
2024-07-29 14:08:32 +0300

    Add posts and home page

--- Keepi/Server/Program.cs
+++ Keepi/Server/Program.cs
@@ -9,17 +9,14 @@ using Keepi.Client.Repositories.Implementation;
 
 var builder = WebApplication.CreateBuilder(args);
 
-
 builder.Services.AddHttpClient();
-//builder.Services.AddScoped<IHttpService, HttpService>();
 
-
-// Add services to the container.
 
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
 builder.Services.AddDbContext<Db_Context>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 
 var app = builder.Build();
 
@@ -42,6 +39,8 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+//app.UseAuthentication();
+//app.UseAuthorization();
 
 app.MapRazorPages();
 app.MapControllers();
@@ -51,19 +50,92 @@ app.Run();
 
 
 
-// Configure the HTTP request pipeline.
-//if (!app.Environment.IsDevelopment())
+
+
+//using Microsoft.AspNetCore.ResponseCompression;
+//using Microsoft.EntityFrameworkCore;
+//using Microsoft.Extensions.DependencyInjection;
+//using Keepi;
+//using Keepi.Server;
+//using Keepi.Client.Communication;
+//using Keepi.Client.Repositories.Interfaces;
+//using Keepi.Client.Repositories.Implementation;
+//using Microsoft.AspNetCore.Identity;
+//using Microsoft.AspNetCore.Authentication;
+
+//var builder = WebApplication.CreateBuilder(args);
+
+
+//builder.Services.AddHttpClient();
+////builder.Services.AddScoped<IHttpService, HttpService>();
+
+
+//// Add services to the container.
+
+//builder.Services.AddControllersWithViews();
+//builder.Services.AddRazorPages();
+//builder.Services.AddDbContext<Db_Context>(options =>
+//    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+//builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+//        .AddEntityFrameworkStores<Db_Context>();
+
+//var builder2 = builder.Services.AddIdentityServer()
+//    .AddApiAuthorization<IdentityUser, Db_Context>();
+
+//// הוספת מפתח זמני - למטרות פיתוח בלבד
+//builder2.AddDeveloperSigningCredential();
+
+//builder.Services.AddAuthentication()
+//    .AddIdentityServerJwt();
+
+
+//var app = builder.Build();
+
+//// Configure the HTTP request pipeline.
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseWebAssemblyDebugging();
+//}
+//else
 //{
 //    app.UseExceptionHandler("/Error");
+//    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
 //    app.UseHsts();
 //}
 
+//app.UseHttpsRedirection();
 
+//app.UseBlazorFrameworkFiles();
+//app.UseStaticFiles();
+
+//app.UseRouting();
+
+//app.UseAuthentication();
+//app.UseIdentityServer();
 //app.UseAuthorization();
 
-//app.MapControllerRoute(
-//    name: "default",
-//    pattern: "{controller=Home}/{action=Index}/{id?}");
 //app.MapRazorPages();
+//app.MapControllers();
+//app.MapFallbackToFile("index.html");
 
 //app.Run();
+
+
+
+//// Configure the HTTP request pipeline.
+////if (!app.Environment.IsDevelopment())
+////{
+////    app.UseExceptionHandler("/Error");
+////    app.UseHsts();
+////}
+
+
+////app.UseAuthorization();
+
+////app.MapControllerRoute(
+////    name: "default",
+////    pattern: "{controller=Home}/{action=Index}/{id?}");
+////app.MapRazorPages();
+
+////app.Run();
